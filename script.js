6540550624b8cf0c8f4d55b4eb1dd3cdcd356127
Andy Stowers <andystowers@MacBook-Pro.local>
2025-12-10 18:26:44 -0300

    fixing next/previous

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const previous = document.querySelector(".previous");
 const next = document.querySelector(".next");
+const previous2 = document.querySelector(".previous2");
+const next2 = document.querySelector(".next2");
 const aboutButton = document.querySelector(".about-button");
 const hideButton = document.querySelector(".hide-button");
 const introText = document.querySelector(".intro-text");
+const revealChad = document.querySelector(".revealChad");
+const hideChad = document.querySelector(".hideChad");
+const revealKink = document.querySelector(".revealKink");
+const hideKink = document.querySelector(".hideKink");
 
 const cardList = [
   "card0",
@@ -17,6 +23,7 @@ const cardList = [
   "card9",
   "card10",
   "card11",
+  "card12",
 ];
 
 var position = 0;
@@ -34,17 +41,52 @@ aboutButton.addEventListener("click", (e) => {
 });
 
 function nextCard() {
-  if (position <= cardList.length) {
+  if (position == 0) {
+    document.querySelector(`.${cardList[position]}`).classList.add("hidden");
+    document
+      .querySelector(`.${cardList[position + 1]}`)
+      .classList.toggle("hidden");
+    previous.classList.remove("hidden");
+    previous2.classList.remove("hidden");
+    position = position + 1;
+  } else if (position + 2 < cardList.length) {
     document.querySelector(`.${cardList[position]}`).classList.toggle("hidden");
     document
       .querySelector(`.${cardList[position + 1]}`)
       .classList.toggle("hidden");
     position = position + 1;
+  } else if (position + 2 == cardList.length) {
+    document.querySelector(`.${cardList[position]}`).classList.toggle("hidden");
+    document
+      .querySelector(`.${cardList[position + 1]}`)
+      .classList.toggle("hidden");
+    position = position + 1;
+    next.classList.toggle("hidden");
+    next2.classList.toggle("hidden");
   }
 }
 
 function previousCard() {
-  if (position > 0) {
+  if (position == cardList.length - 1) {
+    document.querySelector(`.${cardList[position]}`).classList.toggle("hidden");
+    document
+      .querySelector(`.${cardList[position - 1]}`)
+      .classList.toggle("hidden");
+
+    next.classList.toggle("hidden");
+    next2.classList.toggle("hidden");
+    position = position - 1;
+  } else if (position - 1 > 0) {
+    document.querySelector(`.${cardList[position]}`).classList.toggle("hidden");
+    document
+      .querySelector(`.${cardList[position - 1]}`)
+      .classList.toggle("hidden");
+
+    position = position - 1;
+  } else if (position - 1 == 0) {
+    previous.classList.toggle("hidden");
+
+    previous2.classList.toggle("hidden");
     document.querySelector(`.${cardList[position]}`).classList.toggle("hidden");
     document
       .querySelector(`.${cardList[position - 1]}`)
@@ -52,5 +94,23 @@ function previousCard() {
     position = position - 1;
   }
 }
+
+function revealChadFunc() {
+  document.querySelector(".chadJesus").classList.toggle("hidden");
+  document.querySelector(".card1Warning").classList.toggle("hidden");
+}
+function revealKinkFunc() {
+  document.querySelector(".kinkJesus").classList.toggle("hidden");
+  document.querySelector(".card6Warning").classList.toggle("hidden");
+}
+
 next.addEventListener("click", nextCard);
 previous.addEventListener("click", previousCard);
+
+next2.addEventListener("click", nextCard);
+previous2.addEventListener("click", previousCard);
+hideChad.addEventListener("click", nextCard);
+revealChad.addEventListener("click", revealChadFunc);
+
+hideKink.addEventListener("click", nextCard);
+revealKink.addEventListener("click", revealKinkFunc);
